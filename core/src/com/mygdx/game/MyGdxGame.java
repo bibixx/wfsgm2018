fa5daf5359fc0a6b8f0f4cfc0e5b2d7e7226ffde
Kacper Pietrzak <kacpietrzak@gmail.com>
2018-11-10 02:59:06 +0100

    Physics WIP.

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,32 +2,132 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.*;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	public static final float PPM = 32;
+	private boolean DEBUG = false;
+
+	private OrthographicCamera camera;
+
+	private Box2DDebugRenderer b2dr;
+	private World world;
+	private Body player;
+	private Body planet;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, w/2, h/2);
+		Vector3 position = camera.position;
+		position.x = 0;
+		position.y =  100;
+		camera.position.set(position);
+
+		world = new World(new Vector2(0, 0f), false);
+		b2dr = new Box2DDebugRenderer();
+
+		player = createCircle(-100, 140, 32, 32, false);
+		planet = createCircle(0, 100, 64, 32, false);
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 1, 0, 1);
+	public void render() {
+		update(Gdx.graphics.getDeltaTime());
+
+		// Render
+		Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		b2dr.render(world, camera.combined.scl(PPM));
+
+		if(Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) Gdx.app.exit();
 	}
-	
+
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void dispose() {
+		world.dispose();
+		b2dr.dispose();
+	}
+
+	public void update(float delta) {
+		world.step(1 / 60f, 6, 2);
+
+		//inputUpdate(delta);
+		player.setLinearVelocity(1, 0);
+		player.applyForceToCenter(0f, 0, true);
+
+		double distanceSquared = Math.abs(
+				Math.pow(player.getPosition().x - planet.getPosition().x, 2)
+			  + Math.pow(player.getPosition().y - planet.getPosition().y, 2)
+		);
+
+		double planetMass = planet.getMass();
+		double playerMass = player.getMass();
+
+		double constG = 6.67 * Math.pow(10, -11);
+		double force = constG * planetMass * playerMass
+					 / distanceSquared
+		;
+		System.out.println(force);
+
+
+		camera.update();
+	}
+
+	public void inputUpdate(float delta) {
+		int horizontalForce = 0;
+
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			horizontalForce -= 1;
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			horizontalForce += 1;
+		}
+
+		if(Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
+			player.applyForceToCenter(0, 300, false);
+		}
+
+		player.setLinearVelocity(horizontalForce * 5, player.getLinearVelocity().y);
+	}
+
+	public void cameraUpdate() {
+		Vector3 position = camera.position;
+		position.x = player.getPosition().x * PPM;
+		position.y = player.getPosition().y * PPM;
+		camera.position.set(position);
+
+		camera.update();
+	}
+
+	public Body createCircle(int x, int y, int width, int height, boolean isStatic) {
+		Body pBody;
+		BodyDef def = new BodyDef();
+
+		if(isStatic)
+			def.type = BodyDef.BodyType.StaticBody;
+		else
+			def.type = BodyDef.BodyType.DynamicBody;
+
+		def.position.set(x / PPM, y / PPM);
+		def.fixedRotation = true;
+		pBody = world.createBody(def);
+
+		CircleShape shape = new CircleShape();
+		shape.setRadius(width / 4 / PPM);
+
+		pBody.createFixture(shape, 1.0f);
+		shape.dispose();
+		return pBody;
 	}
 }
